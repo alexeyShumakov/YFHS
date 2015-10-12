aa9c5165e365da03ba1bd219a63007edb790f96a
alexeyShumakov <bobshaftoe@yandex.ru>
2015-10-12 22:14:22 +0500

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,46 @@
 require 'faraday'
 require 'json'
-Card.all.each do |card|
-  card.player_class = nil
-  card.save
+Card.delete_all
+
+conn =  Faraday.new(url: 'https://omgvamp-hearthstone-v1.p.mashape.com')
+response = conn.get do |req|
+  req.url '/cards'
+  req.headers['X-Mashape-Key'] = 'QFoPqBzZPBmshJ2Pq9hptKbD5WX0p18oBYFjsnLkliETS6QyN7'
+  req.params['locale'] = 'ruRU'
 end
-PlayerClass.delete_all
-# Card.delete_all
-# conn =  Faraday.new(url: 'https://omgvamp-hearthstone-v1.p.mashape.com')
-# response = conn.get do |req|
-#   req.url '/cards'
-#   req.headers['X-Mashape-Key'] = 'QFoPqBzZPBmshJ2Pq9hptKbD5WX0p18oBYFjsnLkliETS6QyN7'
-#   req.params['locale'] = 'ruRU'
-# end
-# response_json = JSON.parse response.body
-#
-# response_json.each do |_, value|
-#   value.each do |card|
-#
-#     if card.key? 'collectible'
-#       if card['collectible']
-#         if card.key? 'img'
-#           card_model = Card.new
-#           card_model.card_game_id = card['cardId']
-#           card_model.name = card['name']
-#           card_model.card_set = card['cardSet']
-#           card_model.type = card['type']
-#           card_model.faction = card['faction']
-#           card_model.rarity = card['rarity']
-#           card_model.attack = card['attack']
-#           card_model.cost = card['cost']
-#           card_model.health = card['health']
-#           card_model.durability = card['durability']
-#           card_model.text = card['text']
-#           card_model.flavor = card['flavor']
-#           card_model.artist = card['artist']
-#           card_model.collectible = card['collectible']
-#           card_model.elite = card['elite']
-#           card_model.race = card['race']
-#           card_model.player_class = card['playerClass']
-#           card_model.locale = card['locale']
-#           card_model.mechanics = card['mechanics']
-#           card_model.img = URI.parse(card['img'])
-#           card_model.img_gold = URI.parse(card['imgGold'])
-#           puts card['name']
-#           card_model.save
-#         end
-#       end
-#     end
-#   end
-# end
+response_json = JSON.parse response.body
+
+response_json.each do |_, value|
+  value.each do |card|
+
+    if card.key? 'collectible'
+      if card['collectible']
+        if card.key? 'img'
+          card_model = Card.new
+          card_model.card_game_id = card['cardId']
+          card_model.name = card['name']
+          card_model.card_set = card['cardSet']
+          card_model.card_type = card['type']
+          card_model.rarity = card['rarity']
+          card_model.attack = card['attack']
+          card_model.cost = card['cost']
+          card_model.health = card['health']
+          card_model.durability = card['durability']
+          card_model.text = card['text']
+          card_model.flavor = card['flavor']
+          card_model.collectible = card['collectible']
+          card_model.elite = card['elite']
+          card_model.player_class_str = card['playerClass']
+          card_model.mechanics = card['mechanics']
+          card_model.img = URI.parse(card['img'])
+          card_model.img_gold = URI.parse(card['imgGold'])
+          puts card['name']
+          card_model.save
+        end
+      end
+    end
+  end
+end
 
 player_classes = {
   Priest: 'Жрец',
