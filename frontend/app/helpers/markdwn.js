669d5fcb3590c0519feb2e9ed54e30b622e3fb4c
alexeyShumakov <bobshaftoe@yandex.ru>
2015-12-15 14:30:54 +0500

    custom md dialect

--- frontend/app/helpers/markdwn.js
+++ frontend/app/helpers/markdwn.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-
 export function markdwn(params/*, hash*/) {
-  let html = markdown.toHTML(params[0]);
+  let html = markdown.toHTML(params[0], 'Yfhs');
   return Ember.String.htmlSafe(html);
 }
 
